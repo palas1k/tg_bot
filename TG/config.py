ac4d2d7524bc97d158ed669e42fda12722d060c5
Игорь <42812135+igor228337@users.noreply.github.com>
2023-12-25 13:03:14 +0300

    Update config.py

--- TG/config.py
+++ TG/config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "6437988250:AAGJRvRVjLseX9WSOTcwxAISyI7fYCMamI8"
-id_admin = 6044110141
-email_address = "dramaturg2021igor@yandex.ru"
-email_password = "Vfnhtif1"
+BOT_TOKEN = ""
+id_admin = 1
+email_address = ""
+email_password = ""
